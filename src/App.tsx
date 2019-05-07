1a07b831d9ff5d2be00606f2d0a443bbcedf6d04
Gabriel Stein <steindleve@gmail.com>
2019-05-07 11:23:53 -0400

    Setting up dev env

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+  return <div className="App" />;
+};
 
 export default App;
